c331d2adc007b90df04474eeea55b059be9022a2
kldeb <karam.debly@gmail.com>
2024-02-20 16:01:13 -0500

    nicer imports

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,6 +1,6 @@
 import { ApiHandler } from "sst/node/api";
-import { db } from "../../core/src/sql/index";
-import { users } from "../../core/src/sql/schema";
+import { db } from "@drizzle-sst/core/sql";
+import { users } from "@drizzle-sst/core/sql/schema";
 
 export const handler = ApiHandler(async (_evt) => {
   const response = await db.select().from(users);
